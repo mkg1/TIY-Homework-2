c60a75a6f685323dae5c6a2226541498e9d0692b
Molly G <gehring.mg@gmail.com>
2016-02-02 23:16:41 -0500

    Add method attempting to add comment on user behavior

--- guessing_game_hard.rb
+++ guessing_game_hard.rb
@@ -1,7 +1,3 @@
-# def generate_number
-#   return rand(1..100)
-# end
-
 def generate_number
   big_array = (1..100).to_a
   return big_array.sample
@@ -17,17 +13,33 @@ def letters(guess)
   return !(guess.to_i.to_s == guess || guess.to_f.to_s == guess)
 end
 
+# def already_guessed_low(array, guess)
+#   array.each do |x|
+#     if guess < x + 1
+#       return true
+#     end
+#   end
+# end
+
+def already_guessed_low(array, guess)
+   array.each do |x|
+    array.index?(x<guess)
+    return true
+  end
+end
+
 guesses = []
 random = generate_number
+puts "Random number: #{random}"
 count = 0
 number_of_guesses = 5
 
 puts "Please guess a number"
 
 while count <= number_of_guesses
-  count += 1
   original_input = gets.chomp
   user_guess = original_input.to_i
+  count += 1
   if user_guess == random
     puts "NICE!"
     break
@@ -37,9 +49,12 @@ while count <= number_of_guesses
     puts "AHHHHHH NUMBERS PLEASE!"
   elsif count >=  number_of_guesses
     puts "You lose!! Too many guesses!"
+    puts "#{guesses}"
     break
   elsif guesses.include?(user_guess)
     puts "Durrr you already guessed that! Another number please!"
+  elsif already_guessed_low(guesses, user_guess)
+    puts "Silly user - you just wasted a guess. I already indicated that a number above your current guess was too low! Another number please!"
   elsif user_guess < random
     puts "Please guess again - too low! Guess again"
     guesses << user_guess
